33898fd9c2715916e39424ef1cc92846fdd22a2e
Taquispro <121485751+Taquispro@users.noreply.github.com>
2025-03-09 14:58:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -41,4 +41,5 @@ def get_price(listing_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)  # Change port to 8000
+    app.run(host="0.0.0.0", port=8000)  # Run on all network interfaces
+
